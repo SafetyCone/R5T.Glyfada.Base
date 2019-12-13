0839e99b7410b8700e20c60432023875247d6f93
MinexAutomation <minex.automation@gmail.com>
2019-12-12 22:27:49 -0800

    Added GitOperator and GitOperatorCore service definition interface types.

--- source/R5T.Glyfada.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Glyfada.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Glyfada.Base/Code/Services/Implementations/GitOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Glyfada
+{
+    public class GitOperator : IGitOperator
+    {
+        private IGitOperatorCore Core { get; }
+
+
+        public GitOperator(IGitOperatorCore core)
+        {
+            this.Core = core;
+        }
+    }
+}
