ae75fb964f1d044c4c05649ea9a794e3a644534f
MinexAutomation <minex.automation@gmail.com>
2019-12-22 00:15:29 -0800

    Changed capitalization.

--- source/R5T.Glyfada.Base/Code/Constants.cs
+++ source/R5T.Glyfada.Base/Code/Constants.cs
@@ -5,6 +5,8 @@ namespace R5T.Glyfada.Base
 {
     public static class Constants
     {
+        public const string GitName = "Git";
+
         public const string OriginDefaultRemoteRepositoryAlias = "origin";
     }
 }
